03015aca4a5a000303fa3a4ee6f4d420898ac557
Gufo <gufo.dev@gmail.com>
2025-01-19 11:50:09 +0100

    Adjust card width and padding

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,9 @@
 module.exports = {
   ".card": {
-    "@apply relative bg-surface border rounded-xl overflow-hidden": {},
+    "@apply relative w-full bg-surface border rounded-xl overflow-hidden": {},
 
     "&-section": {
-      "@apply p-6 sm:p-8": {},
+      "@apply p-6": {},
     },
   },
 };
