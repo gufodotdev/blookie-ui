7a1c50765e6b65931175c6deb50f982cc9378ed7
Gufo <gufo.dev@gmail.com>
2025-01-25 15:26:44 +0100

    feat(card): add size modifiers

--- src/components/card.js
+++ src/components/card.js
@@ -5,6 +5,19 @@ module.exports = {
   },
 
   ".card-section": {
+    "@apply [.card-section:where(&:not(.card-section-sm,.card-section-base,.card-section-lg))]:card-section-base":
+      {},
+  },
+
+  ".card-section-sm": {
+    "@apply p-4": {},
+  },
+
+  ".card-section-base": {
     "@apply p-6": {},
+  },
+
+  ".card-section-lg": {
+    "@apply p-12": {},
   },
 };
