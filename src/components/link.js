7bcd777cdcc2ddd5b59bbf6aef1197490d91dfd7
Gufo <gufo.dev@gmail.com>
2025-01-25 10:05:44 +0100

    Add default size and variant to link

--- src/components/link.js
+++ src/components/link.js
@@ -1,7 +1,13 @@
 module.exports = {
   ".link": {
-    "@apply inline-flex text-foreground items-center justify-center gap-1.5 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 cursor-pointer hover:opacity-85":
+    "@apply inline-flex items-center justify-center gap-1.5 whitespace-nowrap rounded-md font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 cursor-pointer hover:opacity-85":
       {},
+    "@apply [.link:where(&:not(.link-default,.link-primary))]:link-default": {},
+    "@apply [.link:where(&:not(.link-sm,.link-base,.link-lg))]:link-base": {},
+  },
+
+  ".link-default": {
+    "@apply text-foreground": {},
   },
 
   ".link-primary": {
@@ -10,6 +16,10 @@ module.exports = {
 
   ".link-sm": {
     "@apply text-xs": {},
+  },
+
+  ".link-base": {
+    "@apply text-sm": {},
   },
 
   ".link-lg": {
