d96da739f1fe88036a3956e2142a06be36d0fcaf
Gufo <gufo.dev@gmail.com>
2025-01-25 10:12:20 +0100

    Refactor table component styles and rename classes for consistency

--- src/components/table.js
+++ src/components/table.js
@@ -1,34 +1,30 @@
 module.exports = {
   ".table": {
-    "@apply relative w-full overflow-auto text-sm": {},
+    "@apply w-full overflow-auto text-sm": {},
   },
 
-  ".table-header": {
-    "@apply border-b [&_tr]:border-b": {},
-  },
+  ".table-head": {},
 
   ".table-body": {
     "@apply [&_tr:last-child]:border-0": {},
   },
 
-  ".table.table-striped": {
-    "@apply [&_tr:nth-child(even)]:bg-muted": {},
-  },
-
-  ".table-footer": {
-    "@apply border-t bg-muted/50 font-medium [&>tr]:last:border-b-0": {},
-  },
+  ".table-footer": {},
 
   ".table-row": {
     "@apply border-b": {},
   },
 
-  ".table-head": {
+  ".table-header": {
     "@apply h-12 px-4 text-left align-middle font-medium text-muted-foreground":
       {},
   },
 
-  ".table-cell": {
+  ".table-data": {
     "@apply p-4 align-middle": {},
+  },
+
+  ".table.table-striped": {
+    "@apply [&_tr:nth-child(even)]:bg-muted": {},
   },
 };
