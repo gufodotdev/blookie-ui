f16506ef87870917196854110b14ed7e6b0faec5
Gufo <gufo.dev@gmail.com>
2025-01-28 19:25:53 +0100

    feat(tailwind): set Inter as the default sans-serif font

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         white: "hsla(var(--color-white))",
         black: "hsla(var(--color-black))",
