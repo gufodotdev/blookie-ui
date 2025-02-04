9aaae02c0aa0f52f23b0bdff1c040ef489856d4a
Gufo <gufo.dev@gmail.com>
2025-02-04 07:30:54 +0100

    chore(theme): move animations to extends to preserve defaults

--- src/theme.js
+++ src/theme.js
@@ -62,150 +62,150 @@ module.exports = {
           foreground: "hsla(var(--color-danger-foreground))",
         },
       },
-    },
-    keyframes: {
-      "drawer-content-open-right": {
-        from: {
-          translate: "100% 0%",
+      keyframes: {
+        "drawer-content-open-right": {
+          from: {
+            translate: "100% 0%",
+          },
+          to: {
+            translate: "0% 0%",
+          },
         },
-        to: {
-          translate: "0% 0%",
+        "drawer-content-close-right": {
+          from: {
+            translate: "0% 0%",
+          },
+          to: {
+            translate: "100% 0%",
+          },
+        },
+        "drawer-content-open-left": {
+          from: {
+            translate: "-100% 0%",
+          },
+          to: {
+            translate: "0% 0%",
+          },
+        },
+        "drawer-content-close-left": {
+          from: {
+            translate: "0% 0%",
+          },
+          to: {
+            translate: "-100% 0%",
+          },
+        },
+        "drawer-content-open-top": {
+          from: {
+            translate: "0% -100%",
+          },
+          to: {
+            translate: "0% 0%",
+          },
+        },
+        "drawer-content-close-top": {
+          from: {
+            translate: "0% 0%",
+          },
+          to: {
+            translate: "0% -100%",
+          },
+        },
+        "drawer-content-open-bottom": {
+          from: {
+            translate: "0% 100%",
+          },
+          to: {
+            translate: "0% 0%",
+          },
+        },
+        "drawer-content-close-bottom": {
+          from: {
+            translate: "0% 0%",
+          },
+          to: {
+            translate: "0% 100%",
+          },
+        },
+        "dialog-content-open": {
+          from: {
+            scale: "0.9",
+            translate: "-5% -5%",
+            opacity: "0",
+          },
+          to: {
+            scale: "1",
+            translate: "0% 0%",
+            opacity: "1",
+          },
+        },
+        "dialog-content-close": {
+          from: {
+            scale: "1",
+            translate: "0% 0%",
+            opacity: "1",
+          },
+          to: {
+            scale: "0.9",
+            translate: "-5% -5%",
+            opacity: "0",
+          },
+        },
+        "popover-content-open": {
+          from: {
+            scale: "0.95",
+            opacity: "0",
+          },
+          to: {
+            scale: "1",
+            opacity: "1",
+          },
+        },
+        "popover-content-close": {
+          from: {
+            scale: "1",
+            opacity: "1",
+          },
+          to: {
+            scale: "0.95",
+            opacity: "0",
+          },
+        },
+        "fade-in": {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+        "fade-out": {
+          from: {
+            opacity: "1",
+          },
+          to: {
+            opacity: "0",
+          },
         },
       },
-      "drawer-content-close-right": {
-        from: {
-          translate: "0% 0%",
-        },
-        to: {
-          translate: "100% 0%",
-        },
+      animation: {
+        "dialog-overlay-open": "fade-in 150ms",
+        "dialog-overlay-close": "fade-out 75ms",
+        "dialog-content-open": "dialog-content-open 150ms",
+        "dialog-content-close": "dialog-content-close 75ms",
+        "popover-content-open": "popover-content-open 75ms",
+        "popover-content-close": "popover-content-close 75ms",
+        "drawer-overlay-open": "fade-in 300ms",
+        "drawer-overlay-close": "fade-out 150ms",
+        "drawer-content-open-right": "drawer-content-open-right 300ms",
+        "drawer-content-close-right": "drawer-content-close-right 150ms",
+        "drawer-content-open-left": "drawer-content-open-left 300ms",
+        "drawer-content-close-left": "drawer-content-close-left 150ms",
+        "drawer-content-open-top": "drawer-content-open-top 300ms",
+        "drawer-content-close-top": "drawer-content-close-top 150ms",
+        "drawer-content-open-bottom": "drawer-content-open-bottom 300ms",
+        "drawer-content-close-bottom": "drawer-content-close-bottom 150ms",
       },
-      "drawer-content-open-left": {
-        from: {
-          translate: "-100% 0%",
-        },
-        to: {
-          translate: "0% 0%",
-        },
-      },
-      "drawer-content-close-left": {
-        from: {
-          translate: "0% 0%",
-        },
-        to: {
-          translate: "-100% 0%",
-        },
-      },
-      "drawer-content-open-top": {
-        from: {
-          translate: "0% -100%",
-        },
-        to: {
-          translate: "0% 0%",
-        },
-      },
-      "drawer-content-close-top": {
-        from: {
-          translate: "0% 0%",
-        },
-        to: {
-          translate: "0% -100%",
-        },
-      },
-      "drawer-content-open-bottom": {
-        from: {
-          translate: "0% 100%",
-        },
-        to: {
-          translate: "0% 0%",
-        },
-      },
-      "drawer-content-close-bottom": {
-        from: {
-          translate: "0% 0%",
-        },
-        to: {
-          translate: "0% 100%",
-        },
-      },
-      "dialog-content-open": {
-        from: {
-          scale: "0.9",
-          translate: "-5% -5%",
-          opacity: "0",
-        },
-        to: {
-          scale: "1",
-          translate: "0% 0%",
-          opacity: "1",
-        },
-      },
-      "dialog-content-close": {
-        from: {
-          scale: "1",
-          translate: "0% 0%",
-          opacity: "1",
-        },
-        to: {
-          scale: "0.9",
-          translate: "-5% -5%",
-          opacity: "0",
-        },
-      },
-      "popover-content-open": {
-        from: {
-          scale: "0.95",
-          opacity: "0",
-        },
-        to: {
-          scale: "1",
-          opacity: "1",
-        },
-      },
-      "popover-content-close": {
-        from: {
-          scale: "1",
-          opacity: "1",
-        },
-        to: {
-          scale: "0.95",
-          opacity: "0",
-        },
-      },
-      "fade-in": {
-        from: {
-          opacity: "0",
-        },
-        to: {
-          opacity: "1",
-        },
-      },
-      "fade-out": {
-        from: {
-          opacity: "1",
-        },
-        to: {
-          opacity: "0",
-        },
-      },
-    },
-    animation: {
-      "dialog-overlay-open": "fade-in 150ms",
-      "dialog-overlay-close": "fade-out 75ms",
-      "dialog-content-open": "dialog-content-open 150ms",
-      "dialog-content-close": "dialog-content-close 75ms",
-      "popover-content-open": "popover-content-open 75ms",
-      "popover-content-close": "popover-content-close 75ms",
-      "drawer-overlay-open": "fade-in 300ms",
-      "drawer-overlay-close": "fade-out 150ms",
-      "drawer-content-open-right": "drawer-content-open-right 300ms",
-      "drawer-content-close-right": "drawer-content-close-right 150ms",
-      "drawer-content-open-left": "drawer-content-open-left 300ms",
-      "drawer-content-close-left": "drawer-content-close-left 150ms",
-      "drawer-content-open-top": "drawer-content-open-top 300ms",
-      "drawer-content-close-top": "drawer-content-close-top 150ms",
-      "drawer-content-open-bottom": "drawer-content-open-bottom 300ms",
-      "drawer-content-close-bottom": "drawer-content-close-bottom 150ms",
     },
   },
 };
