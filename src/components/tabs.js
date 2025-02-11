0e9ac098a94edabd662f18396d5b86a42a291344
Gufo <gufo.dev@gmail.com>
2025-02-11 18:43:55 +0100

    feat(tabs): improve style and add new variants

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,15 +1,36 @@
 module.exports = {
+  ".tabs": {
+    "@apply [.tabs:where(&:not(.tabs-boxed,.tabs-underlined))]:tabs-boxed": {},
+  },
+
   ".tabs-list": {
-    "@apply bg-muted text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1":
-      {},
+    "@apply inline-flex": {},
   },
 
   ".tabs-trigger": {
-    "@apply data-[state=active]:bg-surface data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm":
+    "@apply px-4 h-9 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium disabled:pointer-events-none disabled:opacity-50":
       {},
   },
 
   ".tabs-content": {
-    "@apply mt-2": {},
+    "@apply mt-2 w-full": {},
+  },
+
+  ".tabs-boxed": {
+    ".tabs-trigger": {
+      "@apply rounded-md": {},
+      "@apply data-[state=active]:bg-surface data-[state=active]:shadow-sm": {},
+    },
+
+    ".tabs-list": {
+      "@apply bg-muted p-1 rounded-md": {},
+    },
+  },
+
+  ".tabs-underlined": {
+    ".tabs-trigger": {
+      "@apply border-b-2": {},
+      "@apply data-[state=active]:border-foreground": {},
+    },
   },
 };
